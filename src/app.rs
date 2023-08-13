a3f244fb63caec3112a953c8f3ec462787390c04
unknown <gerbenvoogt2@hotmail.com>
2023-08-13 13:22:07 +0000

    implemented app object

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,6 @@
+use crate::course::{self, Course, retrieve_courses_active, COURSES_DIR};
+use tui::widgets::TableState;
+
 #[derive(Debug)]
 pub enum RouteId {
 }
@@ -13,14 +16,54 @@ pub struct Route {
     pub hovered_block: ActiveBlock,
 }
 
-
 pub struct App {
+    pub highlighted_course: Course,
+    pub active_courses: Vec<Course>,
+    pub state: TableState,
 }
 
-
-impl Default for App {
-    fn default() -> App {
+impl App {
+    pub fn new() -> App {
+        let active_courses_list = retrieve_courses_active(COURSES_DIR);
+        let first_course = &active_courses_list[0];
         App {
+            highlighted_course: first_course.clone(),
+            active_courses: active_courses_list,
+            state: TableState::default(),
         }
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.active_courses.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i))
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.active_courses.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i))
+    }
+
+    pub fn update_highlighted_course(&mut self) {
+        let i = self.state.selected().unwrap_or_default();
+        let course_list = self.active_courses.clone();
+        self.highlighted_course = course_list[i].clone();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ mod course;
 mod ui;
 mod app;
 
+// Probably bad practice but w/e it works for now
 use course::*;
 use ui::*;
+use app::*;
 
-use std::{io, fs, process};
+use std::{io, fs, process, os::unix::prelude::PermissionsExt};
 use chrono;
 use tui::{
     backend::{CrosstermBackend, Backend},
@@ -26,34 +28,46 @@ use crossterm::{
 use serde::{Deserialize, Serialize};
 
 
-fn close_application() -> anyhow::Result<()> {
+fn close_application() -> io::Result<()> {
     disable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, LeaveAlternateScreen, DisableMouseCapture)?;
     Ok(())
 }
 
-
-fn main() -> anyhow::Result<()> {
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+fn run_app<B>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<()>
+    where B: Backend, {
 
     loop {
         terminal.draw(|f| {
-            ui(f)
+            ui(f, app)
         })?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Char('q') => break,
+                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Char('j') => app.next(),
+                KeyCode::Char('k') => app.previous(),
+                KeyCode::Enter => app.update_highlighted_course(),
                 _ => {},
             }
         }
     }
+}
+
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout,
+        EnterAlternateScreen,
+        EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    
+    // Create app and run it
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
 
     // Clean up the terminal again
     close_application()?;
@@ -61,6 +75,13 @@ fn main() -> anyhow::Result<()> {
         terminal.backend_mut(),
     )?;
     terminal.show_cursor()?;
+
+    match res {
+        Ok(_) => { },
+        Err(res) => {
+            println!("{:?}", res);
+        }
+    }
 
     Ok(())
 }
